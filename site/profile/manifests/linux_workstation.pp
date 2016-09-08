76a16f01d238d534330746e0b65df3031cb10163
Brian Flannery <brian.flannery@gmail.com>
2016-09-08 07:47:01 -0600

    virtualbox and chrome modules only work in Linux

--- site/profile/manifests/linux_workstation.pp
+++ site/profile/manifests/linux_workstation.pp
@@ -2,33 +2,36 @@ class profile::linux_workstation (
   $package_list = ['nodejs', 'nodejs-legacy', 'npm',],
   $os_specific_packages = true,
 ) {
-  notify { 'profile::linux_workstation': }
-  package { $package_list:
-    ensure => installed,
-  }
-  if $os_specific_packages {
-    case $::osfamily {
-      'Debian': {
-        # package { ['g++', 'ruby-dev', 'lsb-core', ]:
-        package { ['g++', 'ruby-dev', ]:
-          ensure => installed,
-        }
-        case $::operatingsystem {
-          'Ubuntu': {
-            package { ['ecryptfs-utils', ]:
-              ensure => installed,
-            }
-          }
-          default: { warning("Unexpected operatingsystem '${::operatingsystem}'.") }
-        }
-      }
-      'RedHat': {
-        package { ['gcc-c++', 'ruby-devel', ]:
-          ensure => installed,
-        }
-      }
-      default: { warning("Unexpected osfamily '${::osfamily}'.") }
+  if $kernel == "Linux" {
+    package { $package_list:
+      ensure => installed,
     }
+    if $os_specific_packages {
+      case $::osfamily {
+        'Debian': {
+          # package { ['g++', 'ruby-dev', 'lsb-core', ]:
+          package { ['g++', 'ruby-dev', ]:
+            ensure => installed,
+          }
+          case $::operatingsystem {
+            'Ubuntu': {
+              package { ['ecryptfs-utils', ]:
+                ensure => installed,
+              }
+            }
+            default: { warning("Unexpected operatingsystem '${::operatingsystem}'.") }
+          }
+        }
+        'RedHat': {
+          package { ['gcc-c++', 'ruby-devel', ]:
+            ensure => installed,
+          }
+        }
+        default: { warning("Unexpected osfamily '${::osfamily}'.") }
+      }
+    }
+    include ::google_chrome
+    include ::virtualbox
   }
   include ::vagrant
 }
