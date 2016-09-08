afc10780a3e4cd82c3248d3e9cd50aeed221983c
Brian Flannery <brian.flannery@gmail.com>
2016-09-08 00:36:11 -0600

    apply.sh, stop excluding .git

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -28,7 +28,12 @@ puppet_apply() {
   [[ -e $pdir/README.md ]] || { sudo mv $pdir $pdir.orig && sudo mkdir $pdir ; } ;
   local x=`ls -lrt $pdir/ | egrep $'[0-9][ \t][ \t]*root[ \t]'` ;
   [[ -z $x ]] || { echo "NOTE: Found root ownership in pdir, $pdir; chown-ing." ; execute sudo chown -R $USER $pdir ; } ;
-  execute sudo rsync -av . $pdir/ --exclude=.tmp --exclude=.git ;
+
+  # # # .git folder preferable for config_version.sh script:
+  # execute sudo rsync -av . $pdir/ --exclude=.tmp --exclude=.git ;
+  execute sudo rsync -av . $pdir/ --exclude=.tmp ;
+  # # #
+  
   # execute sudo puppet apply -t "$@" --modulepath='site:modules' manifests/site.pp ;
   execute sudo puppet apply -t "$@" manifests/site.pp ;
 }
