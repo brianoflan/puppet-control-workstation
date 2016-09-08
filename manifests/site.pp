5eb36137dc64dfc254f7fbc3c0335696c057c8f6
Brian Flannery <brian.flannery@gmail.com>
2016-09-08 00:18:29 -0600

    site.pp just directs puppet to the workstation role

---
+++ manifests/site.pp
@@ -0,0 +1,42 @@
+## site.pp ##
+
+# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
+# used when an agent connects to a master and asks for an updated configuration.
+#
+# Global objects like filebuckets and resource defaults should go in this file,
+# as should the default node definition. (The default node can be omitted
+# if you use the console and don't define any other nodes in site.pp. See
+# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.)
+
+## Active Configurations ##
+
+# Disable filebucket by default for all File resources:
+#http://docs.puppetlabs.com/pe/latest/release_notes.html#filebucket-resource-no-longer-created-by-default
+File { backup => false }
+
+#Ensure all yumrepo resources are done before any package resources.
+# the exception is for "rpm" providers to a package resource - we want to allow
+# rpm providers before some Package(s) because you might need a repo installer
+# which is sometimes done by RPM
+Yumrepo <| |> -> Package <| provider != 'rpm' |>
+
+#Don't show diff output from augeas, potential sensistive content
+Augeas { show_diff => false }
+
+# DEFAULT NODE
+# Node definitions in this file are merged with node data from the console. See
+# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.
+
+# The default node definition matches any node lacking a more specific node
+# definition. If there are no other nodes in this file, classes declared here
+# will be included in every node's catalog, *in addition* to any classes
+# specified in the console for that node.
+
+node default {
+  include ::profile::base
+  include ::role::workstation
+}
+
+#
